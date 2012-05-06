29fde41f0901516e65ec2e3ed5da44bef1ea6fa1
Scott Kirkpatrick <skirkpatrick4@mail.csuchico.edu>
2012-05-06 16:55:17 -0700

    Added components function
    Added a function to count the number of components in a graph.
    Also changed the Graph typedef to use vecS for the VertexList and
    listS for the OutEdgeList, instead of the other way around.

--- isite.cpp
+++ isite.cpp
@@ -1,15 +1,24 @@
-#include<cstdio>
-#include<boost/graph/adjacency_list.hpp>
+#include<cstdio>    //fscanf can be useful if sites are read as strings
+#include<iostream>  //for input and output
+#include<fstream>   //for file input and output
+#include<utility>   //for std::pair
+#include<vector>    //for std::vector
 
-using namespace boost;
+#include<boost/graph/adjacency_list.hpp>
+#include<boost/graph/connected_components.hpp>
+
 using namespace std;
+using namespace boost;
 
 struct edgesite
 {
-    //How are sites denoted? int/enum/pair
+    pair<int,int> site1;
+    pair<int,int> site2;
 };
 
-typedef adjacency_list<vecS, listS, undirectedS, no_property, edgesite> Graph;
+
+
+typedef adjacency_list<listS, vecS, undirectedS, no_property, edgesite> Graph;
 
 /*
 adjacency_list<OutEdgeList,         <-listS=time, vecS=space
@@ -22,8 +31,13 @@ adjacency_list<OutEdgeList,         <-listS=time, vecS=space
                 */
 
 typedef graph_traits<Graph>::vertex_iterator vertex_iterator;
+typedef graph_traits<Graph>::edge_iterator edge_iterator;
 typedef graph_traits<Graph>::adjacency_iterator AdjIter;
 
+
+/*
+    Counts the number of triangles in a graph
+*/
 int triangles(Graph& graph)
 {
     vertex_iterator vi, viend;
@@ -39,14 +53,18 @@ int triangles(Graph& graph)
             //Check for edges between vertices adjacent to vi aka triangle
             for (adji2=adji/*works with self-loops?*/; adji2!=adjiend; ++adji2)
             {
-                if (edge(*adji, *adji2, graph).second) //since undirected, should only need 1-way, right?
-                    numTriangles++;
+                if (edge(*adji, *adji2, graph).second)
+                numTriangles++;
             }
         }
     }
     return numTriangles/3;
 }
 
+
+/*
+    Counts the number of triples in a graph
+*/
 int triples(Graph& graph)
 {
     vertex_iterator vi, viend;
@@ -55,6 +73,16 @@ int triples(Graph& graph)
     for (tie(vi, viend)=vertices(graph); vi!=viend; vi++)
         numTriples+=((degree(*vi, graph)*(degree(*vi,graph)-1))/2);
     return numTriples;
+}
+
+
+/*
+    Counts the number of components in a graph
+*/
+int components(Graph& graph)
+{
+    vector<int> c(num_vertices(graph));
+    return connected_components(graph, &c[0]);//this is an easier way I found
 }
 
 int main(int argc, char* argv[])
