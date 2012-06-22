7a6dc18eb2f9476e79c0183d807b673c1a93db79
Scott Kirkpatrick <skirkpatrick4@mail.csuchico.edu>
2012-06-22 11:01:47 -0700

    Added error checking and revamped node duplication
    Node duplication is currently not working correctly because duplicated
    edges are not being applied to the iSite of non-duplicated nodes.

--- isite.cpp
+++ isite.cpp
@@ -69,7 +69,7 @@ int triangles(Graph& graph)
     for (tie(vi, viend)=vertices(graph); vi!=viend; vi++)
     {
         //Cycle through adjacent vertices
-        for (tie(adji, adjiend)=adjacent_vertices(*vi, graph); adji!=adjiend; adji++)
+        for(tie(adji,adjiend)=adjacent_vertices(*vi,graph);adji!=adjiend;adji++)
         {
             //Check for edges between vertices adjacent to vi aka triangle
             for (adji2=adji/*works with self-loops?*/; adji2!=adjiend; ++adji2)
@@ -114,62 +114,92 @@ struct parameters
     string infile;
     double prob_loss;
     double prob_asym;
-    int end_order;
-    int iterations;
+    unsigned int end_order;
+    unsigned int iterations;
 } param;
 
-pair<Graph::vertex_descriptor, Graph::vertex_descriptor> duplicate(Graph& graph)
+Graph::vertex_descriptor edgeDest(const Graph::vertex_descriptor vd,
+                                  const Graph::edge_descriptor ed,
+                                  const Graph& graph)
+{
+    return (vd==source(ed, graph)) ? target(ed, graph) : source(ed, graph);
+}
+
+pair<Graph::vertex_descriptor,Graph::vertex_descriptor> duplicate(Graph& graph)
 {
     srand(time(NULL));
-//    int parent = (int)((float)rand()/(RAND_MAX)*(boost::num_vertices(graph)-1) );
     int parent = rand() % (num_vertices(graph)-1);
 //the (graph -1) and (node +1) make it so the range is between 1 to last_node 
     parent+=1;
     cout << "node chosen for duplication (parent): " << parent << endl;
+    Graph::vertex_descriptor parent_description = vertex(parent, graph);
 
-//get a new vertex
+    //get a new vertex
     vertex_iterator child;
     child = boost::add_vertex(graph);
     Graph::vertex_descriptor child_description = *child;
     cout << "duplicated node (child): " << *child << endl;
 
-//give all of the parent edges to the child
+    //copy edges and isites
+    int numSites = graph[parent_description].sites.size();
+    for (int i=0; i < numSites; i++)
+    {
+        isite newSite;
+        newSite.age=0;
+
+        int siteEdges = graph[parent_description].sites[i].edges.size();
+        for (int j=0; j < siteEdges; j++)
+        {
+            
+            Graph::edge_descriptor ed;
+            bool temp_bool;
+            Graph::vertex_descriptor vd = edgeDest(parent_description,
+                graph[parent_description].sites[i].edges[j], graph);
+
+            tie(ed, temp_bool) = add_edge(child_description,vd,graph);
+
+            newSite.edges.push_back(ed);
+            //need to add edge descriptor to other vertex's iSite
+                //possibly use map instead of vector for iSites
+        }
+
+        graph[child_description].sites.push_back(newSite);
+    }
+
+
+/***********Antiquated: replaced by above code
+
+    //give all of the parent edges to the child
     out_edge_iterator oei, oeiend;
     for( tie(oei, oeiend) = out_edges(parent, graph); oei != oeiend; ++oei )
     {
         boost::add_edge(target(*oei,graph), *child, graph);
-        cout << "adding: " << target(*oei,graph) << " to child: " << *child << endl;
+        cout << "adding: " << target(*oei,graph)<<" to child: "<<*child<<endl;
     }
 
-//get vertex_descriptor to parent node
+    //get vertex_descriptor to parent node
     tie(oei, oeiend) = out_edges(parent, graph);
     Graph::vertex_descriptor parent_description = source(*oei, graph);
 
-//clone each of the parent's isites and give it to the child
+    //clone each of the parent's isites and give it to the child
     int numSites = graph[parent_description].sites.size();
     for( int i=0; i < numSites; i++)
     {
-    /*
-        isite* newSite = new isite(); 
-        newSite->age = 0;
-        copy( graph[parent_description].sites[i].edges.begin(), graph[parent_description].sites[i].edges.end(), newSite->edges.begin() ); 
-        graph[child_description].sites.push_back(*newSite);
-        */
         isite newSite;
         newSite.age=0;
-        newSite.edges.insert(newSite.edges.begin(),graph[parent_description].sites[i].edges.begin(),graph[parent_description].sites[i].edges.end());
-        //If we'd rather use copy() instead of insert, we need to first do a
-            //resize() or we get a segfault
+        newSite.edges.insert(newSite.edges.begin(),
+            graph[parent_description].sites[i].edges.begin(),
+            graph[parent_description].sites[i].edges.end());
 
         graph[child_description].sites.push_back(newSite);
-//SHOULD DO MEMORY CLEAN UP HERE???
-    //Got rid of the new operator. No worries.
     }
 
-//return parent, child
+***********************************************/
+
+    //return parent, child
     pair<Graph::vertex_descriptor, Graph::vertex_descriptor> tmpPair;
     tmpPair = std::make_pair(parent_description, child_description);
-return tmpPair; 
+    return tmpPair; 
 }
 
 //Perform iSite algorithm
@@ -214,6 +244,23 @@ int main(int argc, char* argv[])
     param.end_order=atoi(argv[4]);      //Order at which to stop
     param.iterations=atoi(argv[5]);     //Number of times to run algorithm
 
+    //Input validation
+    if (param.prob_loss<0.0 || param.prob_loss>1.0)
+    {
+        cerr<<"Error: Probability of loss must be between 0 and 1"<<endl;
+        exit(1);
+    }
+    if (param.prob_asym<0.0 || param.prob_loss>1.0)
+    {
+        cerr<<"Error: Probability of assymetry must be between 0 and 1"<<endl;
+        exit(1);
+    }
+    if (param.iterations<0)
+    {
+        cerr<<"Error: Number of iterations must be a positive integer"<<endl;
+        exit(1);
+    }
+
     ifstream infile(param.infile.c_str());
     if (!infile)
     {
@@ -255,7 +302,7 @@ int main(int argc, char* argv[])
 	}
 
 #ifdef DEBUG
-    cout << "origional graph" << endl;
+    cout << "original graph" << endl;
     vertex_iterator vi, viend;
     for (tie(vi,viend) = vertices(graph); vi!=viend; ++vi)
     {
@@ -267,58 +314,74 @@ int main(int argc, char* argv[])
     }
 #endif
 
-//possibility of endless loop here on bad user input, should use '<' instead of '!='
-    while (num_vertices(graph)!=param.end_order+1)
-    {
-        duplication(graph);
-        addAge(graph);
-    }
-
-#ifdef DEBUG
-    cout << "graph after duplication of nodes to end_order" << endl;
-    for (tie(vi,viend) = vertices(graph); vi!=viend; ++vi)
-    {
-        cout<<*vi;
-        out_edge_iterator oei, oeiend;
-        for (tie(oei,oeiend)=out_edges(*vi,graph); oei!=oeiend; ++oei)
-            cout<<"->"<<target(*oei,graph);
-        cout<<endl;
-    }
-#endif
-
-#ifdef DEBUG
-    cout << "listing of all nodes. For each node - the isite and the age of that site" << endl;
-    for (tie(vi,viend) = vertices(graph); vi!=viend; ++vi)
-    {
-        Graph::vertex_descriptor vd1 = *vi; 
-        cout<<"node: " << *vi << endl;
-        for (int i=0; i != graph[vd1].sites.size(); i++)
-            cout<<"site: "<< i << " num_edges to site: " << graph[vd1].sites[i].edges.size() << " age of site: " << graph[vd1].sites[i].age << endl;
-        cout<<endl;
-    }
-#endif
-
-
-    //Output
+    //Opening output file
     ofstream outfile("result");
     if (!outfile)
     {
         cerr<<"Error opening output file: result"<<endl;
         exit(1);
     }
+
+    while (param.iterations--)
+    {
+        while (num_vertices(graph)<param.end_order+1)
+        {
+            duplication(graph);
+            addAge(graph);
+#ifdef DEBUG
+            cout << "graph during algorithm" << endl;
+            for (tie(vi,viend) = vertices(graph); vi!=viend; ++vi)
+            {
+                cout<<*vi;
+                out_edge_iterator oei, oeiend;
+                for (tie(oei,oeiend)=out_edges(*vi,graph); oei!=oeiend; ++oei)
+                    cout<<"->"<<target(*oei,graph);
+                cout<<endl;
+            }
+#endif
+
+        }
+
+#ifdef DEBUG
+        cout << "graph after duplication of nodes to end_order" << endl;
+        for (tie(vi,viend) = vertices(graph); vi!=viend; ++vi)
+        {
+            cout<<*vi;
+            out_edge_iterator oei, oeiend;
+            for (tie(oei,oeiend)=out_edges(*vi,graph); oei!=oeiend; ++oei)
+                cout<<"->"<<target(*oei,graph);
+            cout<<endl;
+        }
+#endif
+
+#ifdef DEBUG
+        cout << "listing of all nodes. "
+            "For each node - the isite and the age of that site" << endl;
+        for (tie(vi,viend) = vertices(graph); vi!=viend; ++vi)
+        {
+            Graph::vertex_descriptor vd1 = *vi; 
+            cout<<"node: " << *vi << endl;
+            for (int i=0; i != graph[vd1].sites.size(); i++)
+                cout<<"site: "<< i
+                    <<" num_edges to site: "<<graph[vd1].sites[i].edges.size()
+                    << " age of site: " << graph[vd1].sites[i].age << endl;
+            cout<<endl;
+        }
+#endif
 	
-    outfile<<param.prob_loss<<" ";
-    outfile<<param.prob_asym<<" ";
-    outfile<<num_vertices(graph)-1<<" ";
-    outfile<<num_edges(graph)<<" ";
-    int numTriangles=triangles(graph);
-    int numTriples=countTriples(graph);
-    outfile<<numTriangles<<" ";
-    outfile<<numTriples<<" ";
-    outfile<<(double)(3*numTriangles)/numTriples<<" ";
-    outfile<<components(graph);
+        outfile<<param.prob_loss<<" ";
+        outfile<<param.prob_asym<<" ";
+        outfile<<num_vertices(graph)-1<<" ";
+        outfile<<num_edges(graph)<<" ";
+        int numTriangles=triangles(graph);
+        int numTriples=countTriples(graph);
+        outfile<<numTriangles<<" ";
+        outfile<<numTriples<<" ";
+        outfile<<(double)(3*numTriangles)/numTriples<<" ";
+        outfile<<components(graph);
+        outfile<<endl;
 
-
+    }
 
     outfile.close();
     infile.close();
